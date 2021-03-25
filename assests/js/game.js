8997b6ee00ef13b5aa0591fb79658a4bf7540b2d
unknown <tgelety@gmail.com>
2021-03-24 17:01:42 -0700

    first

---
+++ assests/js/game.js
@@ -0,0 +1,15 @@
+//this is a prompt with a variable//
+
+var playerName = window.prompt("what is your player's name?");
+
+console.log(playerName);
+
+//this is called an expression//
+
+function fight() {
+window.alert("The fight has begun!");
+}
+
+//calling the function//
+
+// fight()
