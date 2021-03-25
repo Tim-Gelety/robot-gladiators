632147c735d44db19ba8d0323befbcc683d0e4ce
unknown <tgelety@gmail.com>
2021-03-24 18:10:07 -0700

    round-frame

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,15 +1,46 @@
-//this is a prompt with a variable//
+var playerName = window.prompt("What is your player's name?");
+var playerHealth = 100;
 
-var playerName = window.prompt("what is your player's name?");
-
-console.log(playerName);
-
-//this is called an expression//
-
-function fight() {
-window.alert("The fight has begun!");
+if (playerHealth > 0){
+    console.log(playerName + " is still alive!")
 }
+var playerAttack = 10;
 
-//calling the function//
+//console.log helps look for messups//
 
-// fight()
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to robot Gladiators!");
+    
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+} 
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left."
+    );
+}
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
+  // Log a resulting message to the console so we know that it worked.
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+); 
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+} 
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left."
+    );
+}
+};
+
+fight();
