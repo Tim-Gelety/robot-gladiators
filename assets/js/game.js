bda98ef354db95658db337c9829d8f50d4b167d1
unknown <tgelety@gmail.com>
2021-03-31 14:30:42 -0700

    randomness

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,15 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10;)
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+   var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +54,8 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -68,8 +70,11 @@ var fight = function (enemyName) {
     }
   }
 };
-
-var startGame = function () {
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * (min -  max + 1) + min);
+  return value ;
+};
+var startGame = function (min, max) {
   // fight each enemy-robot by looping over them and fighting them one at a time
   playerHealth = 100;
   playerAttack = 10;
@@ -85,7 +90,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
